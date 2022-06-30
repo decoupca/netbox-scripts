c62eabf1a5bc29c09b2d03b39d32b5ed0d863c37
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2022-06-30 16:03:22 -0500

    tag external devices

---
+++ tag_external_devices.py
@@ -0,0 +1,56 @@
+from dcim.choices import DeviceStatusChoices
+from dcim.models import Device
+from virtualization.choices import VirtualMachineStatusChoices
+from virtualization.models import VirtualMachine
+from ipam.choices import IPAddressStatusChoices
+from ipam.models import IPAddress
+from extras.scripts import *
+from extras.models import Tag
+import ipaddress
+from typing import Union
+
+
+class TagExternalDevices(Script):
+    class Meta:
+        name = "Tag External Devices"
+        description = "Tag external devices based on IP addresses"
+
+    tag = ObjectVar(
+        model=Tag,
+        description="Apply this tag to any device that has a public IP address on any interface",
+        required=True,
+    )
+
+    def is_public(self, addr: IPAddress) -> bool:
+        addr = ipaddress.IPv4Interface(addr.address)
+        rfc6598 = ipaddress.IPv4Network("100.64.0.0/10")
+        if not addr.is_private and addr not in rfc6598:
+            return True
+        else:
+            return False
+
+    def tag_if_has_public(self, device: Device, data) -> Union[Device, None]:
+        for interface in device.interfaces.all():
+            for addr in interface.ip_addresses.exclude(
+                status=IPAddressStatusChoices.STATUS_DEPRECATED
+            ).all():
+                if self.is_public(addr):
+                    device.tags.add(data["tag"])
+                    device.save()
+                    self.log_info(
+                        f'{device.name} has public IP {addr.address}, tagged {data["tag"]}'
+                    )
+                    return device
+        return None
+
+    def run(self, data, commit):
+        tagged_devices = []
+        for device in (
+            Device.objects.filter(status=DeviceStatusChoices.STATUS_ACTIVE)
+            .prefetch_related("interfaces__ip_addresses")
+            .all()
+        ):
+            tagged = self.tag_if_has_public(device, data)
+            if tagged:
+                tagged_devices.append(tagged)
+        return tagged_devices
